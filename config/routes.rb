ca006c99fc4e1b556eb4e044bec9cb30b3c4157c
keroropper <somato.0311@gmail.com>
2021-01-28 12:27:46 +0900

    いいね保存、削除のみ（アイコンの非同期はまだ）

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,10 @@ Rails.application.routes.draw do
       get 'search'
     end
     resources :comments, only: :create
+    resources :likes, only: [:create, :destroy]
   end
+
+  # post 'like/:id' => 'likes#create', as: 'create_like'
+  # delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
+
 end
